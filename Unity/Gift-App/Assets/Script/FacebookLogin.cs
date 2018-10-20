750fddc232b5ebea14d3b41f7b98d737c02ad63c
chon-sakata <skt97yuma@outlook.jp>
2018-10-20 15:24:10 +0900

    Facebookログイン・ログアウトのしくみ、UserDataの作成

--- Unity/Gift-App/Assets/Script/FacebookLogin.cs
+++ Unity/Gift-App/Assets/Script/FacebookLogin.cs
@@ -1,16 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Facebook.Unity;
+using System.Linq;
 
 public class FacebookLogin : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    private void Awake()
+    {
+        FBInitialize();
+    }
+
+    public void LoginButton()
+    {
+        if (!FB.IsLoggedIn && !UserData.IsCreateUserData())
+        {
+            var list = new List<string> { "public_profile", "user_friends", "user_birthday", "user_gender" };
+            FB.LogInWithReadPermissions(list, result =>
+            {
+                var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+                var userId = aToken.UserId;
+                var Token = aToken.TokenString;
+
+                Debug.Log("Facebook Login UserId: " + userId);
+                Debug.Log("Access Token: " + Token);
+                Debug.Log("Expiration Time: " + aToken.ExpirationTime.ToLocalTime().ToString());
+
+                UserData.Init(aToken);
+            });
+
+        }
+        else
+        {
+            Debug.Log("Facebook: Already have user_information");
+        }
+    }
+
+    public void GetFriends()
+    {
+        if (UserData.IsCreateUserData())
+        {
+            FB.API("/me/friends", HttpMethod.GET, GraphResult =>
+            {
+                ((List<object>)GraphResult.ResultDictionary["data"]).ForEach(c => { Debug.Log(((Dictionary<string, object>)c)["name"]); });
+            }, new Dictionary<string, string> { { "access_token", UserData.userData.userAccessToken } });
+        }
+    }
+
+    public void LogoutButton()
+    {
+        if (FB.IsLoggedIn)
+        {
+            FB.LogOut();
+            Debug.Log("Facebook: User Logout");
+        }else
+        {
+            Debug.Log("Facebook: Please Login");
+        }
+        UserData.userData.DestroyUserData();
+    }
+
+    private void FBInitialize()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                {
+                    FB.ActivateApp();
+                }
+            },
+            isShown=> {
+
+            });
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+    }
 }
---
+++ Unity/Gift-App/Assets/Script/UserData.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+
+public class UserData : ScriptableObject {
+    string _userId;
+    string _userAccessToken;
+    string _userName;
+
+    static UserData _userData = null;
+
+    public static UserData userData{
+        get {
+            if (_userData != null)
+            {
+                //Debug.Log("UserData: Find userData");
+                return _userData;
+            }
+            else
+            {
+                if (PlayerPrefs.GetString("user_local_token", "") == "")
+                {
+                    //Debug.Log("UserData: No SaveData");
+                    return null;
+                }
+                else
+                {
+                    //Debug.Log("UserData: Reload SaveData");
+                    _userData = CreateInstance<UserData>();
+                    _userData._userId = PlayerPrefs.GetString("user_local_ID", "");
+                    _userData._userAccessToken = PlayerPrefs.GetString("user_local_token", "");
+                    return _userData;
+                }
+            }
+        }
+    }
+
+    public string userId
+    {
+        get
+        {
+            return this._userId;
+        }
+    }
+
+    public string userAccessToken
+    {
+        get
+        {
+            return this._userAccessToken;
+        }
+    }
+
+    public static void Init(Facebook.Unity.AccessToken aToken)
+    {
+        if (_userData == null)
+        {
+            _userData = UserData.CreateInstance<UserData>();
+            _userData._userId = aToken.UserId;
+            _userData._userAccessToken = aToken.TokenString;
+            PlayerPrefs.SetString("user_local_ID", _userData._userId);
+            PlayerPrefs.SetString("user_local_token", _userData._userAccessToken);
+        }
+    }
+
+    public void DestroyUserData()
+    {
+        _userData = null;
+        PlayerPrefs.DeleteAll();
+    }
+
+    public static bool IsCreateUserData()
+    {
+        return (userData != null);
+    }
+
+}
