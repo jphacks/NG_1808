500d847db5551e0666cd43cdb51df9fdedac7579
kuwayamatakuya <mulberrymountain1126@gmail.com>
2018-10-21 06:07:38 +0900

    add ニフクラに好みデータなどを送るクラス

---
+++ Unity/Gift-App/Assets/Script/WarehouseMan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarehouseMan : MonoBehaviour
+{
+    void Start()
+    {
+        var pref = new UserPreference("bongteng16", "くわ", "sometoken1234", "{\"meat\":5, \"fish\":3}");
+        pref.Save();
+        var pref2 = new UserPreference("bongteng", "わわ", "anothertoken456", "{}");
+        pref.Save();
+
+        pref2.Print();
+    }
+
+    void Update()
+    {
+
+    }
+}
