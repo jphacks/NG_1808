5d7f912670ccb7d55037f24d7aee042aea529d20
kuwayamatakuya <mulberrymountain1126@gmail.com>
2018-10-21 12:03:43 +0900

    add カレンダーの日付選択で記念日表示を切り替える機能（未完成）

---
+++ Unity/Gift-App/Assets/Script/HistoryView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HistoryView : MonoBehaviour
+{
+    [SerializeField]
+    Calendar calender;
+    List<HistoryNode> nodes;
+    // Use this for initialization
+    void Start()
+    {
+        this.nodes = GetComponentsInChildren<HistoryNode>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PickupNodes()
+    {
+        int month = this.calender.targetDate.Month;
+        int day = this.calender.targetDate.Day;
+        foreach (var n in this.nodes)
+        {
+            n.gameObject.SetActive(n.IsSelected(month, day));
+        }
+    }
+}
